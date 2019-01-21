88d2f0643ffc6b1f0d5f26e9835741bc832aad55
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-21 15:30:07 -0500

    Featured Section Completed

---
+++ src/components/layout/main/FeaturedSection/FeaturedSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FeaturedProduct from "./FeaturedProduct";
+const FeaturedSection = props => {
+  return (
+    <div className="title-wrapper  basel-title-color-default basel-title-style-cross basel-title-size-default text-center vc_custom_1453814964817">
+      <span className="title-subtitle font-default">MADE THE HARD WAY</span>
+      <div className="liner-continer">
+        {" "}
+        <span className="left-line" />
+        <h4 className="title">
+          FEATURED PRODUCTS
+          <span className="title-separator">
+            <span />
+          </span>
+        </h4>{" "}
+        <span className="right-line" />
+      </div>
+
+      <FeaturedProduct />
+      <FeaturedProduct />
+      <FeaturedProduct />
+      <FeaturedProduct />
+      <FeaturedProduct />
+      <FeaturedProduct />
+      <FeaturedProduct />
+      <FeaturedProduct />
+    </div>
+  );
+};
+
+export default FeaturedSection;
