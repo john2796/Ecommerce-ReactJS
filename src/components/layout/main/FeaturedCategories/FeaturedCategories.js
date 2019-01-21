2149dec49fdd2b6596872c368c1cbaaccce30459
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-21 16:34:27 -0500

    About section completed

---
+++ src/components/layout/main/FeaturedCategories/FeaturedCategories.js
@@ -0,0 +1,204 @@
+import React from "react";
+import "./FeaturedCategories.scss";
+
+const FeaturedCategories = props => {
+  return (
+    <div className="vc_row wpb_row vc_row-fluid vc_custom_1455123507718">
+      <div className="wpb_column vc_column_container vc_col-sm-12">
+        <div className="vc_column-inner">
+          <div className="wpb_wrapper">
+            <div className="title-wrapper  basel-title-color-default basel-title-style-cross basel-title-size-default text-center vc_custom_1453400301393">
+              <span className="title-subtitle font-default">
+                MADE THE HARD WAY
+              </span>
+              <div className="liner-continer">
+                <span className="left-line" />
+                <h4 className="title">
+                  FEATURED CATEGORIES
+                  <span className="title-separator">
+                    <span />
+                  </span>
+                </h4>
+                <span className="right-line" />
+              </div>
+            </div>
+            <div className="vc_row wpb_row vc_inner vc_row-fluid">
+              <div className="wpb_column vc_column_container vc_col-sm-1 vc_col-lg-3 vc_col-md-2 vc_hidden-xs">
+                <div className="vc_column-inner">
+                  <div className="wpb_wrapper" />
+                </div>
+              </div>
+              <div className="wpb_column vc_column_container vc_col-sm-10 vc_col-lg-6 vc_col-md-8 vc_col-xs-12">
+                <div className="vc_column-inner">
+                  <div className="wpb_wrapper">
+                    <div className="wpb_text_column wpb_content_element vc_custom_1455116120654">
+                      <div className="wpb_wrapper">
+                        <p
+                          className="font-title"
+                          style={{
+                            textAlign: "center",
+                            fontSize: "16px",
+                            fontStyle: "italic"
+                          }}
+                        >
+                          Basel &amp; Co. is a powerful eCommerce theme for
+                          WordPress. Visit our shop page to see all main
+                          features for{" "}
+                          <a
+                            className="color-primary"
+                            style={{ textDecoration: "underline" }}
+                            href="http://demo.xtemos.com/basel/shop/"
+                            target="_blank"
+                            rel="noopener"
+                          >
+                            Your Store
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="wpb_column vc_column_container vc_col-sm-1 vc_col-lg-3 vc_col-md-2 vc_hidden-xs">
+                <div className="vc_column-inner">
+                  <div className="wpb_wrapper" />
+                </div>
+              </div>
+            </div>
+            <div
+              className="woocommerce row categories-style-masonry-first categories-masonry categories-space-30 columns-4"
+              style={{ position: "relative", height: "585px" }}
+            >
+              <div
+                className=" col-xs-6 col-sm-4 col-md-6 first  category-grid-item cat-design-default product-category product first"
+                data-loop="1"
+                style={{ position: "absolute", left: "0px", top: "0px" }}
+              >
+                <div className="category-content">
+                  <a
+                    href="https://demo.xtemos.com/basel/product-category/bags/"
+                    className="category-link"
+                  >
+                    <div className="product-category-thumbnail">
+                      <img
+                        src="https://xtemos2.r.worldssl.net/basel/wp-content/uploads/2015/08/cat-bag-5-546x546.jpg"
+                        alt="Bags"
+                        width="546"
+                      />
+                    </div>
+                    <span className="products-cat-number"> 10 products </span>
+                  </a>
+                  <div className="hover-mask">
+                    <a
+                      href="https://demo.xtemos.com/basel/product-category/bags/"
+                      className="category-link-overlay"
+                    />
+                    <h3> Bags</h3>
+                    <a href="https://demo.xtemos.com/basel/product-category/bags/">
+                      View products
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div
+                className=" col-xs-6 col-sm-4 col-md-3 category-grid-item cat-design-default product-category product"
+                data-loop="2"
+                style={{ position: "absolute", left: "585px", top: "0px" }}
+              >
+                <div className="category-content">
+                  <a
+                    href="https://demo.xtemos.com/basel/product-category/shoes/"
+                    className="category-link"
+                  >
+                    <div className="product-category-thumbnail">
+                      <img
+                        src="https://xtemos2.r.worldssl.net/basel/wp-content/uploads/2015/08/boot-category-1-3.jpg"
+                        alt="Shoes"
+                        width="273"
+                      />
+                    </div>
+                    <span className="products-cat-number"> 10 products </span>
+                  </a>
+                  <div className="hover-mask">
+                    <a
+                      href="https://demo.xtemos.com/basel/product-category/shoes/"
+                      className="category-link-overlay"
+                    />
+                    <h3> Shoes</h3>
+                    <a href="https://demo.xtemos.com/basel/product-category/shoes/">
+                      View products
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div
+                className=" col-xs-6 col-sm-4 col-md-3 category-grid-item cat-design-default product-category product"
+                data-loop="3"
+                style={{ position: "absolute", left: "877px", top: "0px" }}
+              >
+                <div className="category-content">
+                  <a
+                    href="https://demo.xtemos.com/basel/product-category/watches/"
+                    className="category-link"
+                  >
+                    <div className="product-category-thumbnail">
+                      <img
+                        src="https://xtemos3.r.worldssl.net/basel/wp-content/uploads/2015/08/cat-watch-2.jpg"
+                        alt="Watches"
+                        width="273"
+                      />
+                    </div>
+                    <span className="products-cat-number"> 10 products </span>
+                  </a>
+                  <div className="hover-mask">
+                    <a
+                      href="https://demo.xtemos.com/basel/product-category/watches/"
+                      className="category-link-overlay"
+                    />
+                    <h3> Watches</h3>
+                    <a href="https://demo.xtemos.com/basel/product-category/watches/">
+                      View products
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div
+                className=" col-xs-6 col-sm-4 col-md-3 last  category-grid-item cat-design-default product-category product last"
+                data-loop="4"
+                style={{ position: "absolute", left: "585px", top: "292px" }}
+              >
+                <div className="category-content">
+                  <a
+                    href="https://demo.xtemos.com/basel/product-category/woman/"
+                    className="category-link"
+                  >
+                    <div className="product-category-thumbnail">
+                      <img
+                        src="https://xtemos3.r.worldssl.net/basel/wp-content/uploads/2015/08/wooman-cat.jpg"
+                        alt="Woman"
+                        width="273"
+                      />
+                    </div>
+                    <span className="products-cat-number"> 10 products </span>
+                  </a>
+                  <div className="hover-mask">
+                    <a
+                      href="https://demo.xtemos.com/basel/product-category/woman/"
+                      className="category-link-overlay"
+                    />
+                    <h3> Woman</h3>
+                    <a href="https://demo.xtemos.com/basel/product-category/woman/">
+                      View products
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedCategories;
--- src/components/layout/main/Main.js
+++ src/components/layout/main/Main.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import NewsLetter from "./NewsLetter/NewsLetter";
 import FeaturedSection from "./FeaturedSection/FeaturedSection";
+import FeaturedCategories from "./FeaturedCategories/FeaturedCategories";
 import About from "./About/About";
 class Main extends Component {
   render() {
     return (
       <div>
         <h1>Main Components</h1>
+        <FeaturedCategories />
         <FeaturedSection />
         <NewsLetter />
         <About />
